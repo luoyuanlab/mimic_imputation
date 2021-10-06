09e95e0a2f9d2502b139df97b7e260e366c800c4
Yuan Luo <yuanluo@users.noreply.github.com>
2021-10-05 20:03:22 -0500

    Update evalChallengeTest.R

--- src/evalChallengeTest.R
+++ src/evalChallengeTest.R
@@ -3,10 +3,11 @@ evalChallengeTest <- function(tgt, naidx, dnimp) {
 
     timp = list()
     for (i in pts.te) {
-        fn = sprintf('%s/%d.csv', dnimp, i)
-        timp[[i]] = t(read.csv(fn))
+        fn = sprintf('%s/%s.csv', dnimp, i)
+        timp[[i]] = t(read.csv(fn, row.names = 1, header = FALSE))
+        rownames(timp[[i]]) <- 1:nrow(timp[[i]])
     }
-    names(timp) = as.character(1:length(timp))
+    # names(timp) = as.character(1:length(timp))
 
     nrmse = evalPtTensorImpImportTrTeChallenge(timp, tgt, naidx, metric='RMSE range norm')
     cat(sprintf('%s nRMSE\n', dnimp))
